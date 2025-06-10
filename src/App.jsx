e21fe226508dddf8b0b45ebce260f6f9f033b7c2
kmj <myng25@naver.com>
2025-06-10 18:20:22 +0900

    init: 초기 설정

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className ="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        Setting up Tailwind CSS with Vite
+      </div>
+    </>
+  )
+}
+
+export default App
