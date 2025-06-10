4dd11f8d0b2771430ac49d44639c892113accf75
kmj <myng25@naver.com>
2025-06-11 04:39:16 +0900

    지도 페이지 디자인 수정

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -108,7 +108,7 @@ export default function Map() {
   }, []);
 
   return (
-    <div className="pt-10 px-4">
+    <div className="pt-4 text-center px-4">
       <h2 className="text-lg font-bold mb-4">📍 지도에서 보기</h2>
       <div id="map" className="w-full h-[500px] rounded-md shadow border" />
     </div>
