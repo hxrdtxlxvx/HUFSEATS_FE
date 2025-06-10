2e7612b30e1f2dad5d392fa8b7d84d195c0ad34d
kmj <myng25@naver.com>
2025-06-11 00:04:50 +0900

    App.jsx 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
 import Roulette from './pages/Roulette.jsx'
@@ -5,15 +6,15 @@ import Header from './components/Header.jsx'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div>
         <Header />
         <div className="pt-12">
           <Roulette />
         </div>
       </div>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
