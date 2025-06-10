b42bf12002bb0f642a8138d58df98ec394116783
kmj <myng25@naver.com>
2025-06-11 00:34:41 +0900

    /map 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Roulette from './pages/Roulette.jsx'
 import Header from './components/Header.jsx'
+import Map from './pages/Map.jsx'
 
 function App() {
   return (
@@ -10,7 +10,10 @@ function App() {
       <div>
         <Header />
         <div className="pt-12">
-          <Roulette />
+          <Routes>
+            <Route path="/" element={<Roulette />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
